20cb3ad5be3a791dd001d3b999e13c5e2459da06
zigzag <447587073@qq.com>
2017-02-16 11:23:48 +0800

    add vue-router

---
+++ src/module/spa/spa.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+// 引入 饿了么UI
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+// 引入 vue-router
+import VueRouter from 'vue-router'
+
+import App from './App'
+import Use from './user/user'
+
+Vue.use(Element)
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    component: Use
+}, {
+    path: '/use',               // 如何使用
+    component: App,
+    children: [{
+        path: 'start',
+        component: Use
+    }]
+}]
+
+const router = new VueRouter({
+    // mode: 'history',
+    // base: __dirname,
+    routes
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(Use)
+})
---
+++ src/module/spa/user/user.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="word">
+    <h1>{{ msg }}</h1>
+    <h2>Mgt Project with element-ui</h2>
+    <h2>show vue-router and vuex [user]</h2>
+</template>
+
+<script>
+export default {
+  name: 'word',
+  data () {
+    return {
+      msg: 'Welcome to Your mgt App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.word{
+    text-align: center
+}
+</style>
