e8fced06c2ce2ae3441b0d8965337569aff6bb9f
SmallW <447587073@qq.com>
2018-12-10 10:39:41 +0800

    change var to const

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,74 +1,78 @@
-var path = require('path')
-var config = require('../config')
-var utils = require('./utils')
-var projectRoot = path.resolve(__dirname, '../')
-var glob = require('glob');
-var entries = getEntry('./src/module/**/*.js'); // 获得入口js文件
+const path = require("path");
+const config = require("../config");
+const utils = require("./utils");
+const projectRoot = path.resolve(__dirname, "../");
+const glob = require("glob");
+const entries = getEntry("./src/module/**/*.js"); // 获得入口js文件
 
-var autoprefixer = require('autoprefixer');
-var nested = require('postcss-nested');
-var cssnext = require('postcss-cssnext');
-var flexFallback = require('postcss-flex-fallback');
+const autoprefixer = require("autoprefixer");
+const nested = require("postcss-nested");
+const cssnext = require("postcss-cssnext");
+const flexFallback = require("postcss-flex-fallback");
 
-var env = process.env.NODE_ENV
+const env = process.env.NODE_ENV;
 // check env & config/index.js to decide weither to enable CSS Sourcemaps for the
-// various preprocessor loaders added to vue-loader at the end of this file
-var cssSourceMapDev = (env === 'development' && config.dev.cssSourceMap)
-var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap)
-var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
+// constious preprocessor loaders added to vue-loader at the end of this file
+const cssSourceMapDev = env === "development" && config.dev.cssSourceMap;
+const cssSourceMapProd =
+  env === "production" && config.build.productionSourceMap;
+const useCssSourceMap = cssSourceMapDev || cssSourceMapProd;
 
 module.exports = {
   entry: entries,
   output: {
     path: config.build.assetsRoot,
-    publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
-    filename: '[name].js'
+    publicPath:
+      process.env.NODE_ENV === "production"
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath,
+    filename: "[name].js"
   },
   resolve: {
-    extensions: ['', '.js', '.vue'],
-    fallback: [path.join(__dirname, '../node_modules')],
+    extensions: ["", ".js", ".vue"],
+    fallback: [path.join(__dirname, "../node_modules")],
     alias: {
-      'vue$': 'vue/dist/vue',
-      'src': path.resolve(__dirname, '../src'),
-      'common': path.resolve(__dirname, '../src/common'),
-      'components': path.resolve(__dirname, '../src/components')
+      vue$: "vue/dist/vue",
+      src: path.resolve(__dirname, "../src"),
+      common: path.resolve(__dirname, "../src/common"),
+      components: path.resolve(__dirname, "../src/components")
     }
   },
   resolveLoader: {
-    fallback: [path.join(__dirname, '../node_modules')]
+    fallback: [path.join(__dirname, "../node_modules")]
   },
   module: {
     loaders: [
       {
         test: /\.vue$/,
-        loader: 'vue'
+        loader: "vue"
       },
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: "babel",
         include: projectRoot,
         exclude: /node_modules/
       },
       {
         test: /\.json$/,
-        loader: 'json'
+        loader: "json"
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url',
+        loader: "url",
         query: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          name: utils.assetsPath("img/[name].[hash:7].[ext]")
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url',
+        loader: "url",
         query: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath("fonts/[name].[hash:7].[ext]")
         }
-      },
+      }
       //在原有基础上加上一个postcss的loader就可以了
       // {
       //   test:/\.css$/,
@@ -82,20 +86,24 @@ module.exports = {
     autoprefixer: true,
     postcss: [
       nested(),
-      cssnext({ browsers: ['last 2 versions', 'Android >= 2.1', 'iOS >= 7.0'] }),
-      flexFallback(),
+      cssnext({
+        browsers: ["last 2 versions", "Android >= 2.1", "iOS >= 7.0"]
+      }),
+      flexFallback()
     ]
   }
-}
+};
 
 function getEntry(globPath) {
   var entries = {},
-    basename, tmp, pathname;
+    basename,
+    tmp,
+    pathname;
 
-  glob.sync(globPath).forEach(function (entry) {
+  glob.sync(globPath).forEach(function(entry) {
     basename = path.basename(entry, path.extname(entry));
-    tmp = entry.split('/').splice(-3);
-    pathname = tmp.splice(0, 1) + '/' + basename; // 正确输出js和html的路径
+    tmp = entry.split("/").splice(-3);
+    pathname = tmp.splice(0, 1) + "/" + basename; // 正确输出js和html的路径
     entries[pathname] = entry;
   });
 
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,43 +1,46 @@
-var path = require('path');
-var config = require('../config')
-var webpack = require('webpack')
-var merge = require('webpack-merge')
-var utils = require('./utils')
-var baseWebpackConfig = require('./webpack.base.conf')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var glob = require('glob')
+const path = require("path");
+const config = require("../config");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const utils = require("./utils");
+const baseWebpackConfig = require("./webpack.base.conf");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const glob = require("glob");
 
 // add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
-})
+Object.keys(baseWebpackConfig.entry).forEach(function(name) {
+  baseWebpackConfig.entry[name] = ["./build/dev-client"].concat(
+    baseWebpackConfig.entry[name]
+  );
+});
 
 module.exports = merge(baseWebpackConfig, {
   module: {
     loaders: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
   // eval-source-map is faster for development
-  devtool: '#eval-source-map',
+  devtool: "#eval-source-map",
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': config.dev.env
+      "process.env": config.dev.env
     }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ]
-})
+});
 
 function getEntry(globPath) {
   var entries = {},
-    basename, tmp, pathname;
+    basename,
+    tmp,
+    pathname;
 
-  glob.sync(globPath).forEach(function (entry) {
-
+  glob.sync(globPath).forEach(function(entry) {
     basename = path.basename(entry, path.extname(entry));
-    tmp = entry.split('/').splice(-3);
-    pathname = tmp.splice(0, 1) + '/' + basename; // 正确输出js和html的路径
+    tmp = entry.split("/").splice(-3);
+    pathname = tmp.splice(0, 1) + "/" + basename; // 正确输出js和html的路径
 
     entries[pathname] = entry;
   });
@@ -45,21 +48,20 @@ function getEntry(globPath) {
   return entries;
 }
 
-var pages = getEntry('./src/module/**/*.html');
+const pages = getEntry("./src/module/**/*.html");
 
 for (var pathname in pages) {
   // 配置生成的html文件，定义路径等
   var conf = {
-    filename: pathname + '.html',
-    template: pages[pathname],   // 模板路径
-    inject: true,              // js插入位置
+    filename: pathname + ".html",
+    template: pages[pathname], // 模板路径
+    inject: true, // js插入位置
     // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-    chunksSortMode: 'dependency'
-
+    chunksSortMode: "dependency"
   };
 
   if (pathname in module.exports.entry) {
-    conf.chunks = ['manifest', 'vendor', pathname];
+    conf.chunks = ["manifest", "vendor", pathname];
     conf.hash = true;
   }
 
